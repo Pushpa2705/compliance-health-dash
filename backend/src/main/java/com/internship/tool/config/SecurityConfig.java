d0406a81c942810e8deed56851475e60b6740d3a
SUJAN GANIGA <sujan.ganiga2020@gmail.com>
2026-04-24 17:28:28 +0530

    Day 5 — Implemented JWT Authentication with custom filter and secured endpoints

--- backend/src/main/java/com/internship/tool/config/SecurityConfig.java
+++ backend/src/main/java/com/internship/tool/config/SecurityConfig.java
@@ -1,20 +1,29 @@
 package com.internship.tool.config;
 
+import com.internship.tool.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
